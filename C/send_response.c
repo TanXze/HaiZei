f73bdca817509a1028654002ebaec4150548753f
TanXze <2406577693@qq.com>
2018-09-30 16:41:24 +0800

    新文件：   C Pi.log 	新文件：   C send_response.c 	新文件：   C write_Pi_log.c

---
+++ C/send_response.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: send_response.c
+	> Author: Tanxiaoze
+	> Mail: 2406577693@qq.com
+	> Created Time: 2018年09月30日 星期日 16时35分42秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int send_response(int socket, int req) {
+    
+}
