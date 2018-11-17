8acd65eb366bc1c99976003a482951d871ac2d7d
TanXze <2406577693@qq.com>
2018-11-18 01:15:53 +0800

    C

--- C/client.c
+++ C/client.c
@@ -1,10 +1,3 @@
-/*************************************************************************
-	> File Name: socket_client.c
-	> Author: tanxiaoze
-	> Mail: 2406577693@qq.com
-	> Created Time: 2018年09月23日 星期日 16时43分25秒
- ************************************************************************/
-
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <dirent.h>
@@ -24,122 +17,24 @@
 #include <time.h>
 #include <stdarg.h>
 
-#define MAX_SIZE 1024
+#define BUFFER_SIZE 1024
 #define FILE_NAME_MAX_SIZE 512
 
 int main(int argc, char *argv[]) {
-    int sock_client;
-    char buffer[MAX_SIZE];
-    struct sockaddr_in server_addr;
-    int port = atoi(argv[2]);
-    char *host = argv[1];
-
+    int sock_client, sockfd;
+    struct sockaddr_in dest_addr;
     if ((sock_client = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket");
-        return -1;
+        return -1;        
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(host);
-
-    if (connect(sock_client, (struct sockaddr * ) & server_addr, sizeof(server_addr)) < 0) {
-        perror("Connect");
-        return -1;
-    }
-    printf("Connect success!\n");
-
-    /*while (1) {
-        scanf("%[^\n]s\n", buffer);
-        getchar();
-        send(sock_client, buffer, strlen(buffer), 0);
-        bzero(buffer, MAX_SIZE);
-    }*/
-
-    while (1) {
-        char buf_send[20], buf_recv[20];
-        strcpy(buf_send, "100");
-        int sock_short = socket(AF_INET, SOCK_STREAM, 0);
-        connect(sock_short, (struct sockaddr *) & server_addr, sizeof(server_addr));
-        send(sock_short, buf_send, sizeof(buf_send), 0);
-        bzero(buf_send, sizeof(buf_send));
-        int sock_recv = recv(sock_short, buf_recv, sizeof(buf_recv), 0);
-        if (sock_recv = 0) {
-            printf("Server close\n");
-            return -1;
-        }
-        if (sock_recv > 0) {
-            printf("%s", buf_recv);
-            if (strcmp(buf_recv, "1") == 0) {
-                FILE *fcpu = popen("~/HaiZei/Monitoring_system/CPU.sh","r");
-                fgets(buffer, sizeof(buffer), fcpu);
-                send(sock_short, buffer, sizeof(buffer), 0);
-                bzero(buffer, sizeof(buffer));
-                close(sock_short);
-            }
-        }
-        
-        /*strcpy(buf_send, "101");
-        int sock_shor = socket(AF_INET, SOCK_STREAM, 0);
-        connect(sock_shor, (struct sockaddr *) & server_addr, sizeof(server_addr));
-        send(sock_shor, buf_send, sizeof(buf_send), 0);
-        bzero(buf_send, sizeof(buf_send));
-        if (recv(sock_shor, buf_recv, sizeof(buf_recv), 0) > 0) {
-            if (strcmp(buf_recv, "2") == 0) {
-                FILE *fdisk = popen("~/HaiZei/Monitoring_system/disk.sh", "r");
-                fgets(buffer, sizeof(buffer), fdisk);
-                send(sock_client, buffer, sizeof(buffer), 0);
-                bzero(buffer, sizeof(buffer));
-                close(sock_shor);
-            }
-        }
-
-        strcpy(buf_send, "102");
-        int sock_sho = socket(AF_INET, SOCK_STREAM, 0);
-        connect(sock_sho, (struct sockaddr *) & server_addr, sizeof(server_addr));
-        send(sock_sho, buf_send, sizeof(buf_send), 0);
-        bzero(buf_send, sizeof(buf_send));
-        if (recv(sock_sho, buf_recv, sizeof(buf_recv), 0) > 0) {
-            if (strcmp(buf_recv, "3") == 0) {
-                FILE *fmem = popen("~/HaiZei/Monitoring_system/Memlog.sh", "r");
-                fgets(buffer, sizeof(buffer), fmem);
-                send(sock_client, buffer, sizeof(buffer), 0);
-                bzero(buffer, sizeof(buffer));
-                close(sock_sho);
-            }
-        }*/
-        
-        sleep(5);
-    }
-
-    /*while (1) {
-        char file_name[FILE_NAME_MAX_SIZE + 1];
-        bzero(file_name, FILE_NAME_MAX_SIZE + 1);
-        printf("Please input file name on server : \t");
-        strncpy(file_name, buffer, strlen(buffer) > FILE_NAME_MAX_SIZE ? FILE_NAME_MAX_SIZE : strlen(buffer));
-        scanf("%s", file_name);
-
-        FILE *fp = fopen(file_name, "r");
-        if (NULL == fp) {
-            printf("File: %s Not Found!\n", file_name);
-        } else {
-            bzero(buffer, MAX_SIZE);
-            while (!feof(fp)) {
-                int num_fread = fread(buffer, sizeof(char), 1, fp);
-                if (num_fread < 0) {
-                    perror("Fread");
-                    return -1;
-                }
-                send(sock_client, buffer, num_fread, 0);
-                bzero(buffer, MAX_SIZE);
-                close(sock_client);
-            }
-
-            fclose(fp);
-            printf("Receive file:\t%s successful!\n", file_name);
-
-        }
-    }*/
-    close(sock_client);
-    return 0;
+    int port = atoi(argv[2]);
+    char *host = argv[1];
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(port);
+    dest_addr.sin_addr.s_addr = inet_addr(host);
+        if (connect(sock_client, (struct sockaddr * ) & dest_addr, sizeof(dest_addr))) {
+    	    perror("Connect");
+    	    return -1;            
+       }
 }
--- C/master.c
+++ C/master.c
@@ -1,43 +1,88 @@
-/*************************************************************************
-	> File Name: master.c
-	> Author: Tanxiaoze
-	> Mail: 2406577693@qq.com
-	> Created Time: 2018年11月13日 星期二 19时50分09秒
- ************************************************************************/
-
 #include <stdio.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>		
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <signal.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
 
-int main() {
+#define BUFFER_SIZE 1024
+#define FILE_NAME_MAX_SIZE 512
+
+int sock_server, sockfd;
+char buffer[BUFFER_SIZE];
+struct sockaddr_in server_addr, client_addr;
+
+int main(int argc, char *argv[]) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket");
+        return -1;
+    }
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind");
+        return -1;
+    }
+
+    if (listen(sockfd, 20) < 0) {
+        perror("Listen");
+        return -1;
+    }
+    int port = atoi(argv[1]);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    int sock_listen = sock_server;
     pid_t pid;
-    /*for (int i = 0; i < 5; i++) {
-        if ((pid = fork()) == 0) {
-            printf("Child\n");
-            return 0;
-        }
-        sleep(1);
-    }*/
     int n, x, y;
     scanf("%d", &n);
     x = n % 5;
-    if (x != 0) {
-        y = n / 5 + 1;
+    if (x == 0) {
+	y = n / 5;
     } else {
-        y = n / 5;
+	y = n / 5 + 1;
     }
+    while (1) {
         for (int i = 0; i < 5; i++) {
             pid = fork();
             if (pid == 0 || pid == -1) break;
-            n -= 2;
+	    exit(0);
         }
         if (pid == -1) {
             printf("Fork() Error\n");
         } else if (pid == 0) {
             for (int j = 0; j < y; j++) { 
-                printf("Pi%d\n", n--);
+		socklen_t size = sizeof(client_addr);
+        	sock_server = accept(sock_listen, (struct sockaddr *) &client_addr, &size);
+        	if (sock_server < 0) {
+                    perror("Accept");
+            	    break;
+        	}
+
+        	struct sockaddr_in peer;
+        	socklen_t len = sizeof(peer);
+        	bzero(&peer, sizeof(peer));
+        	getpeername(sock_server, (struct sockaddr *) &peer, &len);
+        	char buff_peer[64] = {'\0'};
+        	inet_ntop(AF_INET, (void *)&peer.sin_addr, buff_peer, 63);
+        	printf("%s已连接:\n", buff_peer);	        
             }
-            close();
         }
+	close(sock_server);
+    }
     return 0;
 }
